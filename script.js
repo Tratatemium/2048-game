deef931e960b6a09d50f889153cc9ae03b6a53ec
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-10-14 13:42:22 +0200

    Add initial HTML, CSS, and JavaScript files for 2048 game

---
+++ script.js
@@ -0,0 +1,15 @@
+// #region FUNCTIONS
+
+    const updateGameField = () => {
+
+        for (let i = )
+    }
+
+// #endregion FUNCTIONS
+
+gameArray = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
