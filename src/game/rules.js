35fda393d6b86133eccc6a055b15f6acd6ae05b5
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-13 21:28:32 +0100

    refactor: simplify playerLost logic and enhance merges checking

--- src/game/rules.js
+++ src/game/rules.js
@@ -1,35 +1,23 @@
 import { WIN_TILE } from "../config.js";
+import { transpose } from "../utils/helpers.js";
 
 const playerWon = (gameArray) =>
   gameArray.some((row) => row.some((el) => el.value === WIN_TILE));
 
-const playerLost = () => {
-  // Step 1: Check if there are any empty spaces
-  const emptySpacesExist = state.gameArray.some((row) =>
-    row.some((el) => el.value === 0),
-  );
-
-  // If empty spaces exist, game is not over
-  if (emptySpacesExist) return false;
-
-  // Step 2: Check if any adjacent tiles can be merged (horizontal)
-  for (let i = 0; i < state.gameArray.length; i++) {
-    for (let j = 0; j < state.gameArray.length - 1; j++) {
-      if (state.gameArray[i][j].value === state.gameArray[i][j + 1].value)
-        return false;
+const mergesPossibleInRows = (gameArray) => {
+  for (const row of gameArray) {
+    for (let i = 0; i < row.length - 1; i++) {
+      if (row[i].value === row[i + 1].value) {
+        return true;
+      }
     }
   }
-
-  // Step 3: Check if any adjacent tiles can be merged (vertical)
-  for (let i = 0; i < state.gameArray.length - 1; i++) {
-    for (let j = 0; j < state.gameArray.length; j++) {
-      if (state.gameArray[i][j].value === state.gameArray[i + 1][j].value)
-        return false;
-    }
-  }
-
-  // No empty spaces and no possible merges = game over
-  return true;
+  return false;
 };
+
+const playerLost = (gameArray) =>
+  !gameArray.some(row => row.some(tile => tile.value === 0)) &&
+  !mergesPossibleInRows(gameArray) &&
+  !mergesPossibleInRows(transpose(gameArray));
 
 export { playerWon, playerLost };
